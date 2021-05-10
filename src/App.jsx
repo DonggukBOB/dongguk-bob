6a246582cf259dff763633d1fc52a18f21bfe40a
hwookim <gusdn9754@gmail.com>
2021-05-10 20:44:14 +0900

    feat: 각 페이지 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
+import MainPage from "./pages/main";
+import RecommendPage from "./pages/recommend";
+import ListPage from "./pages/list";
+import MapPage from "./pages/map";
 
 function App() {
-  return <div className="App">DonggukBOB</div>;
+  return (
+    <div className="App">
+      <Route path="/" exact component={MainPage} />
+      <Route path="/recommend" component={RecommendPage} />
+      <Route path="/list" component={ListPage} />
+      <Route path="/map" component={MapPage} />
+    </div>
+  );
 }
 
 export default App;
