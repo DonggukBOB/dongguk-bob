1f69265f5a159d56438521105bfd28d26c6b2575
GyeongYeonAn <ruddus7444@naver.com>
2021-05-15 15:13:36 +0900

    chore: add text

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function MainPage() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <h3>지친 당신의 허기를 달래줄 든-든한</h3>
+      <h1>동국밥</h1>
+      <button onClick={() => alert("recommend로 이동")}>메뉴 추천</button>
+      <button onClick={() => alert("list로 이동")}>맛집 목록</button>
+    </div>
+  );
 }
