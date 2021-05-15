8d37733fca5b6ec45205fb4cf8173eff8e93f4e8
GyeongYeonAn <ruddus7444@naver.com>
2021-05-15 23:08:49 +0900

    chore: image 추가

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import imgMain from "./imgMain.jpg";
 
 export default function MainPage() {
   return (
     <div>
       <h3>지친 당신의 허기를 달래줄 든-든한</h3>
       <h1>동국밥</h1>
+      <img src={imgMain} width="450" height="450" />
       <button onClick={() => alert("recommend로 이동")}>메뉴 추천</button>
       <button onClick={() => alert("list로 이동")}>맛집 목록</button>
     </div>
