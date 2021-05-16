4e488b231314a7082334d3ffae7bf01009ef0c9e
win9612 <mingyu1218@naver.com>
2021-05-16 21:43:53 +0900

    chore: BigButton, DecoImage 컴포넌트 추가 및 임시 저장, chore: Go! 버튼 구현

--- src/pages/recommend/index.jsx
+++ src/pages/recommend/index.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 import Header from "../../components/Header";
+import DecoImage from "./DecoImage";
+import BigButton from "./BigButton";
+
+const Container = styled.div`
+  border: 5px solid #ffcc5c;
+  display: flex;
+  align-items: flex-start;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
 
 export default function RecommendPage() {
   return (
     <>
       <Header />
-      <div>Recommend</div>
+      <Container>
+        <DecoImage />
+        <DecoImage />
+        <DecoImage />
+      </Container>
+      <BigButton />
+      <Container>
+        <DecoImage />
+        <DecoImage />
+        <DecoImage />
+      </Container>
     </>
   );
 }
---
+++ src/pages/recommend/result.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ResultPage() {
+    return (
+      <>
+        <Header />
+        <div>Result will Show Here</div>
+      </>
+    );
+  }
+  
