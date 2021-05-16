013b8d4e842f269cfb6b11e380cc8d65d4283785
GyeongYeonAn <ruddus7444@naver.com>
2021-05-16 21:28:24 +0900

    style: 적절한 위치 지정  - 컴포넌트 크기 및 위치 조정  chore: 버튼에 링크 이동 구현

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,14 +1,62 @@
 import React from "react";
 import imgMain from "./imgMain.jpg";
+import styled from "styled-components";
+import Header from "../../components/Header";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Button = styled.button`
+  width: 200px;
+  height: 200px;
+  border-radius: 5px;
+  background-color: #f29300;
+  border: none;
+  font-size: 25px;
+  margin: 10px;
+  color: #fff;
+`;
+
+const Image = styled.img`
+  width: 400px;
+  height: 400px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Title = styled.h1`
+  letter-spacing: 10px;
+  font-size: 40pt;
+`;
+
+const SubTitle = styled.h3`
+  text-align: center;
+  margin-top: 30px;
+`;
+
+const SubSubTitle = styled.h3`
+  text-align: center;
+`;
 
 export default function MainPage() {
   return (
-    <div>
-      <h3>지친 당신의 허기를 달래줄 든-든한</h3>
-      <h1>동국밥</h1>
-      <img src={imgMain} width="450" height="450" />
-      <button onClick={() => alert("recommend로 이동")}>메뉴 추천</button>
-      <button onClick={() => alert("list로 이동")}>맛집 목록</button>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <SubTitle>지친 당신의 허기를 달래줄</SubTitle>
+        <SubSubTitle>든-든한</SubSubTitle>
+        <Title>동국밥</Title>
+        <Image src={imgMain} />
+        <Link to="../recommend/index.jsx">
+          <Button>메뉴 추천</Button>
+        </Link>
+        <Link to="../list/index.jsx">
+          <Button>맛집 목록</Button>
+        </Link>
+      </Container>
+    </>
   );
 }
