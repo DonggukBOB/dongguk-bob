6a754c26e1cb0ea39dbc51502661ba62740ff18f
GyeongYeonAn <ruddus7444@naver.com>
2021-05-20 01:11:07 +0900

    style: css 수정 - flex를 이용한 가로, 세로 가운데 정렬 - 폰트 사이즈 em 활용하는 것으로 변경 - 전체를 Container로 묶음

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,62 +1,55 @@
 import React from "react";
-import imgMain from "./imgMain.jpg";
+import imgMain from "../../assets/imgMain.jpg";
 import styled from "styled-components";
-import Header from "../../components/Header";
 import { Link } from "react-router-dom";
 
 const Container = styled.div`
   text-align: center;
+  font-size: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10%;
 `;
 
 const Button = styled.button`
-  width: 200px;
-  height: 200px;
   border-radius: 5px;
   background-color: #f29300;
   border: none;
-  font-size: 25px;
-  margin: 10px;
-  color: #fff;
+  font-size: 1.5em;
+  margin: 3%;
+  color: white;
 `;
 
 const Image = styled.img`
-  width: 400px;
-  height: 400px;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
+  width: 100%;
+  height: 100%;
+  margin: auto;
 `;
 
 const Title = styled.h1`
   letter-spacing: 10px;
-  font-size: 40pt;
+  font-size: 2.5em;
 `;
 
 const SubTitle = styled.h3`
-  text-align: center;
-  margin-top: 30px;
-`;
-
-const SubSubTitle = styled.h3`
-  text-align: center;
+  font-size: 1em;
 `;
 
 export default function MainPage() {
   return (
-    <>
-      <Header />
-      <Container>
-        <SubTitle>지친 당신의 허기를 달래줄</SubTitle>
-        <SubSubTitle>든-든한</SubSubTitle>
-        <Title>동국밥</Title>
-        <Image src={imgMain} />
-        <Link to="../recommend/index.jsx">
-          <Button>메뉴 추천</Button>
-        </Link>
-        <Link to="../list/index.jsx">
-          <Button>맛집 목록</Button>
-        </Link>
-      </Container>
-    </>
+    <Container>
+      <SubTitle>지친 당신의 허기를 달래줄</SubTitle>
+      <SubTitle>든-든한</SubTitle>
+      <Title>동국밥</Title>
+      <Image src={imgMain} />
+      <Link to="/recommend">
+        <Button>메뉴 추천</Button>
+      </Link>
+      <Link to="/list">
+        <Button>맛집 목록</Button>
+      </Link>
+    </Container>
   );
 }
