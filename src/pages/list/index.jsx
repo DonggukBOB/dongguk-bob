970d804065b7a3d0307f6ae3cbf5a01995878693
GyeongYeonAn <ruddus7444@naver.com>
2021-05-21 22:17:29 +0900

    chore: divide section(filter, section) and pull data image

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Header from "../../components/Header";
+import styled from "styled-components";
+import places from "../../data/places";
+
+const FilterSection = styled.div`
+  height: 10%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background: grey;
+`;
+
+const List = styled.ul`
+  height: 90%;
+  width: 100%;
+`;
+
+const ListItme = styled.li`
+  padding: 18px;
+`;
 
 export default function ListPage() {
+  const listItems = places.map((post) => (
+    <ListItme key={post.name}>
+      <img src={post.photo} width="100%" />
+    </ListItme>
+  ));
+
   return (
     <>
       <Header />
-      <div>List</div>
+      <FilterSection></FilterSection>
+      <List>{listItems}</List>
     </>
   );
 }
