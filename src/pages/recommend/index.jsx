e0205be12effa32bef0779e2330ec44a7677a076
win9612 <mingyu1218@naver.com>
2021-05-21 14:21:29 +0900

    PR반영 2차 수정, RecommendButton 컴포넌트 삭제 및 통합

--- src/pages/recommend/index.jsx
+++ src/pages/recommend/index.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
-import RecommendButton from "./RecommendButton";
 
 const Container = styled.div`
-  aling-items: center;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
   margin: auto;
+  width: 2.3em;
+  height: 2.3em;
+  border: 10px solid #ea5413;
+  border-radius: 16px;
+  background-color: #fbc402;
+  font-size: 7.2em;
+  font-weight: bold;
 `;
 
 export default function RecommendPage() {
   return (
-    <>
+    <Container>
       <Header />
-      <Container>
-        <RecommendButton />
-      </Container>
-    </>
+      <Button onClick={() => alert("추천 완료!")}>Go!</Button>
+    </Container>
   );
 }
