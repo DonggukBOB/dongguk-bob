41e17dd2f6e4a9c8c9e7c6ef97febe75faaa99a4
GyeongYeonAn <ruddus7444@naver.com>
2021-05-24 10:22:26 +0900

    style: 리스트 요소 적절한 위치로 배치

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -20,12 +20,79 @@ const List = styled.ul`
 
 const ListItme = styled.li`
   padding: 18px;
+  border-bottom: 1px solid #dbdbdb;
+`;
+
+const Thumb = styled.div`
+  position: relative;
+  overflow: hidden;
+  background-size: cover;
+  background-position: 50% 50%;
+  content: "";
+  width: 100%;
+  padding-top: 42.85714%;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  color: rgba(255, 255, 255, 0);
+  text-indent: -9999px;
+  border: none;
+  vertical-align: top;
+`;
+
+const Info = styled.div`
+  padding: 12px 7px 16px 0px;
+  position: relative;
+  text-decoration: none;
+  vertical-align: middle;
+`;
+
+const Title = styled.span`
+  font-size: 1.2em;
+`;
+
+const Score = styled.span`
+  color: #ff792a;
+  position: static;
+  font-size: 1.3em;
+  margin-left: 8px;
+`;
+
+const Address = styled.p`
+  font-size: 0.75rem;
+  margin-top: 3px;
+  color: #9b9b9b;
+  line-height: 1.3em;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding-right: 10px;
+`;
+
+const Review = styled.p`
+  margin-top: 8px;
+  font-size: 0.875rem;
+  line-height: 21px;
 `;
 
 export default function ListPage() {
   const listItems = places.map((post) => (
     <ListItme key={post.name}>
-      <img src={post.photo} width="100%" />
+      <Thumb>
+        <Image src={post.photo} />
+      </Thumb>
+      <Info>
+        <Title>{post.name}</Title>
+        <Score>{post.score}/5</Score>
+        <Address>{post.address}</Address>
+        <Review>{post.comment}</Review>
+      </Info>
     </ListItme>
   ));
 
