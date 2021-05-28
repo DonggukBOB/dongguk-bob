0f9393ff373cc5da514f65cbb12a42d08f91a9e2
GyeongYeonAn <ruddus7444@naver.com>
2021-05-28 13:10:56 +0900

    refactor: 코드 개선 - Image <- Thumnail 네이밍 변경 - 불필요한 스타일 코드 제거 - ListItem 공통 컴포넌트로 분리 - Filter 삭제 (feature/26에서 개발)

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.li`
+  padding: 18px;
+  border-bottom: 1px solid #dbdbdb;
+`;
+
+const Thumbnail = styled.img`
+  width: 100%;
+  height: 144px;
+  object-fit: cover;
+`;
+
+const Info = styled.div`
+  padding: 12px 7px 16px 0px;
+`;
+
+const Title = styled.span`
+  font-size: 1.2em;
+`;
+
+const Score = styled.span`
+  color: #ff792a;
+  font-size: 1.3em;
+  margin-left: 8px;
+`;
+
+const Address = styled.p`
+  font-size: 0.75rem;
+  margin-top: 8px;
+  color: #9b9b9b;
+`;
+
+const Review = styled.p`
+  margin-top: 10px;
+  font-size: 0.875rem;
+`;
+
+export default function ListItem(place) {
+  return (
+    <Container key={place.name}>
+      <Thumbnail src={place.photo} />
+      <Info>
+        <Title>{place.name}</Title>
+        <Score>{place.score}/5</Score>
+        <Address>{place.address}</Address>
+        <Review>{place.comment}</Review>
+      </Info>
+    </Container>
+  );
+}
--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,73 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
-import Filter from "../../components/Filter";
-import styled from "styled-components";
+import ListItem from "../../components/ListItem";
 import places from "../../data/places";
-
-const ListItme = styled.li`
-  padding: 18px;
-  box-sizing: border-box;
-  border-bottom: 1px solid #dbdbdb;
-`;
-
-const Image = styled.img`
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 144px;
-  object-fit: cover;
-  color: rgba(255, 255, 255, 0);
-  text-indent: -9999px;
-  border: none;
-  vertical-align: top;
-`;
-
-const Info = styled.div`
-  padding: 12px 7px 16px 0px;
-`;
-
-const Title = styled.span`
-  font-size: 1.2em;
-`;
-
-const Score = styled.span`
-  color: #ff792a;
-  font-size: 1.3em;
-  margin-left: 8px;
-`;
-
-const Address = styled.p`
-  font-size: 0.75rem;
-  margin-top: 8px;
-  color: #9b9b9b;
-`;
-
-const Review = styled.p`
-  margin-top: 10px;
-  font-size: 0.875rem;
-  line-height: 21px;
-`;
-
-function ListItems(place) {
-  return (
-    <ListItme key={place.name}>
-      <Image src={place.photo} />
-      <Info>
-        <Title>{place.name}</Title>
-        <Score>{place.score}/5</Score>
-        <Address>{place.address}</Address>
-        <Review>{place.comment}</Review>
-      </Info>
-    </ListItme>
-  );
-}
 
 export default function ListPage() {
   return (
     <>
       <Header />
-      <Filter />
-      <ul>{places.map((place) => ListItems(place))}</ul>
+      <ul>{places.map((place) => ListItem(place))}</ul>
     </>
   );
 }
