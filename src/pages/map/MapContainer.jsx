7cc73dcbf6678b243f440ab8f6b734989218280b
win9612 <mingyu1218@naver.com>
2021-05-30 14:56:25 +0900

    feat: 마커 추가, 인포윈도 기본 구현

--- src/pages/map/MapContainer.jsx
+++ src/pages/map/MapContainer.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from "react";
+import places from "../../data/places.js";
 
 const { kakao } = window;
 
-const MapContainer = () => {
+export default function MapContainer() {
   useEffect(() => {
+    mapscript();
+  }, []);
+
+  const mapscript = () => {
     const container = document.getElementById("list-map");
     const options = {
       center: new kakao.maps.LatLng(37.5581, 126.9983),
@@ -11,10 +16,54 @@ const MapContainer = () => {
     };
 
     const map = new kakao.maps.Map(container, options);
-
     const zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-  }, []);
+
+    places.forEach((el) => {
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(
+          el.location.latitude,
+          el.location.longitude
+        ),
+        title: el.name,
+      });
+
+      var infowindow = new kakao.maps.InfoWindow({
+        content: el.name,
+      });
+
+      kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(map, marker, infowindow)
+      );
+      kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infowindow)
+      );
+
+      function makeInfoWindow(map, marker, infowindow) {
+        return function () {
+          infowindow.open(map, marker);
+        };
+      }
+      makeInfoWindow(map, marker, infowindow);
+    });
+
+    function makeOverListener(map, marker, infowindow) {
+      return function () {
+        infowindow.open(map, marker);
+      };
+    }
+
+    function makeOutListener(infowindow) {
+      return function () {
+        infowindow.close();
+      };
+    }
+  };
   return (
     <div
       id="list-map"
@@ -24,6 +73,4 @@ const MapContainer = () => {
       }}
     ></div>
   );
-};
-
-export default MapContainer;
+}
