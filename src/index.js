d30721c09cd14b8c572ab3a1ded7b4b4c0da8a00
hwookim <gusdn9754@gmail.com>
2021-06-02 13:32:17 +0900

    fix: 모바일 화면에서 상태표시줄을 전체 높이에서 제외

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+(() => {
+  const setFullHeightProperty = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
+  setFullHeightProperty();
+  window.addEventListener("resize", setFullHeightProperty);
+})();
+
 ReactDOM.render(
   <BrowserRouter basename={"/dongguk-bob"}>
     <App />
