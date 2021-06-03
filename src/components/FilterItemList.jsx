eb7f5cd34fd2cd3324db8258ddc3c886cc9db3a2
hwookim <gusdn9754@gmail.com>
2021-06-03 23:12:21 +0900

    feat: FilterButton 클릭 시 활성화/비활성화 스타일 변경

--- src/components/FilterItemList.jsx
+++ src/components/FilterItemList.jsx
@@ -1,8 +1,18 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+
+const activeStyle = css`
+  background: #f29300;
+  color: #fff;
+`;
+
+const inactiveStyle = css`
+  background: transparent;
+  color: black;
+`;
 
 const Container = styled.div`
-  background-color: #f9f9f9;
+  background: #f9f9f9;
 `;
 
 const FilterButton = styled.button`
@@ -10,13 +20,27 @@ const FilterButton = styled.button`
   padding: 15px;
   border: none;
   font-size: 0.875rem;
+  ${({ isActive }) => (isActive ? activeStyle : inactiveStyle)}
 `;
 
 export default function FilterItemList({ items }) {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const activateButton = ({ target }) => {
+    const item = target.innerText;
+    item === activeButton ? setActiveButton(null) : setActiveButton(item);
+  };
+
   return (
     <Container>
       {items.map((item) => (
-        <FilterButton key={item}>{item}</FilterButton>
+        <FilterButton
+          key={item}
+          isActive={item === activeButton}
+          onClick={activateButton}
+        >
+          {item}
+        </FilterButton>
       ))}
     </Container>
   );
