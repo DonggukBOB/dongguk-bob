d0002e855cca26b825dbe3d3897960091a3f602e
GyeongYeonAn <ruddus7444@naver.com>
2021-06-05 14:56:35 +0900

    hotfix: 데이터 이름 문제 해결, 남산골, 홍탁집 주소 해결

--- src/pages/recommend/index.jsx
+++ src/pages/recommend/index.jsx
@@ -7,7 +7,7 @@ import PlaceInfoModal, {
   openPlaceInfoModal,
 } from "../../components/PlaceInfoModal";
 
-import PLACES from "../../data/PLACES";
+import PLACES from "../../data/places.js";
 
 const Container = styled.div`
   display: flex;
